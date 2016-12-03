cc413c79426cdc39a66fd044ccd158b965bfb2c0
Rink Springer <rink@rink.nu>
2016-12-03 11:25:32 +0100

    Disable Trace()-ing by default and allow enable/disabling

--- src/trace.cc
+++ src/trace.cc
@@ -7,30 +7,50 @@ namespace Trace {
 
 namespace {
 
-#define REPORT(TYPE) \
-	va_list va; \
-	va_start(va, fmt); \
-	fprintf(stderr, "%s: ", TYPE); \
-	vfprintf(stderr, fmt, va); \
-	fprintf(stderr, "\n"); \
-	va_end(va)
+unsigned int s_TraceMask = 0;
+
+#define REPORT(CATEGORY, TYPE) \
+	if (s_TraceMask & (1 << static_cast<unsigned int>(CATEGORY))) { \
+		va_list va; \
+		va_start(va, fmt); \
+		fprintf(stderr, "%s: ", TYPE); \
+		vfprintf(stderr, fmt, va); \
+		fprintf(stderr, "\n"); \
+		va_end(va); \
+	}
 
 } // unnamed namespace
 
 void Info(const char* fmt, ...) {
-	REPORT("Info");
+	REPORT(C_Info, "Info")
 }
 
 void Error(const char* fmt, ...) {
-	REPORT("Error");
+	REPORT(C_Error, "Error")
 }
 
 void Warning(const char* fmt, ...) {
-	REPORT("Warning");
+	REPORT(C_Warning, "Warning")
 }
 
 void Debug(const char* fmt, ...) {
-	REPORT("Debug");
+	REPORT(C_Debug, "Debug")
+}
+
+void Enable(Category category) {
+	s_TraceMask |= 1 << static_cast<unsigned int>(category);
+}
+
+void Disable(Category category) {
+	s_TraceMask &= ~(1 << static_cast<unsigned int>(category));
+}
+
+void EnableAll() {
+	s_TraceMask = static_cast<unsigned int>(-1);
+}
+
+void DisableAll() {
+	s_TraceMask = 0;
 }
 
 } // namespace Trace
--- src/trace.h
+++ src/trace.h
@@ -6,10 +6,23 @@
 namespace RSSH {
 namespace Trace {
 
+enum Category {
+	C_Info,
+	C_Error,
+	C_Warning,
+	C_Debug
+};
+
 void Info(const char* fmt, ...);
 void Error(const char* fmt, ...);
 void Warning(const char* fmt, ...);
 void Debug(const char* fmt, ...);
+
+void Enable(Category category);
+void Disable(Category category);
+
+void EnableAll();
+void DisableAll();
 
 } // namespace Trace
 } // namespace RSSH
